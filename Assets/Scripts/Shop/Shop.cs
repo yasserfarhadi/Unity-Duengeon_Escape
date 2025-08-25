101fbc7eae07df160c34440f232cf8f91146501c
Yasser Farhadi <yasser.farhadee@gmail.com>
2025-08-26 00:37:37 +0330

    shop ui

---
+++ Assets/Scripts/Shop/Shop.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+
+	[SerializeField] GameObject _shopPanel;
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player" && _shopPanel)
+		{
+			Player player = other.GetComponent<Player>();
+			if (player)
+			{
+				UIManager.Instance.OpenShop(player.Diamonds);
+			}
+			_shopPanel.SetActive(true);
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.tag == "Player" && _shopPanel)
+		{
+			_shopPanel.SetActive(false);
+		}
+	}
+
+	public void SelectItem()
+	{
+		Debug.Log("asdawd");
+	}
+}
---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+	private static UIManager _instance;
+	public static UIManager Instance
+	{
+		get
+		{
+			if (_instance == null) Debug.LogError("UI Manager is NULL");
+			return _instance;
+		}
+	}
+
+	[SerializeField] Text _gemText;
+	[SerializeField] Image _selectionImg;
+	// private Player _player;
+
+	public void OpenShop(int gemCount)
+	{
+		_gemText.text = gemCount + " Gem" + (gemCount == 1 ? "" : "s");
+	}
+
+	private void Awake()
+	{
+		_instance = this;
+		// _player = GameObject.Find("Player").GetComponent<Player>();
+	}
+}
